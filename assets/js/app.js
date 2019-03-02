6a033b942d941432f1930fa6f9760a38b6772175
Riwandy <riwandy10@gmail.com>
2019-03-02 17:42:53 +0700

    update code

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,9 @@ let scores = [0, 0]; //membuat variable score yg diisi array dengan nilai 0
 let roundScore = 0;
 let activePlayer = 0;
 const dice = Math.floor(Math.random() * 6) + 1;
-const currentScoreP1 = document.querySelector('#current-0').textContent = dice; // menampilkan current score diplayer 1
-const imgDice = document.querySelector('.dice').style.display = 'none';
-const btn_roll = document.querySelector('.btn-roll').addEventListener('click')
+const currentScoreP1 = document.querySelector('#current-0');
+const imgDice = document.querySelector('.dice');
+const btn_roll = document.querySelector('.btn-roll');
 
+currentScoreP1.textContent = dice; // menampilkan current score diplayer 1
+imgDice.style.display = 'none'; //menyembunyikan gambar
